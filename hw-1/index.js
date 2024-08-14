2bfdb856bcb7493dcb068f63ad780058184901b7
Sanzhar <thecodefather.kg@gmail.com>
2024-08-13 20:54:36 -0500

    hw-1

--- hw-1/index.js
+++ hw-1/index.js
@@ -1,39 +1,56 @@
-// to render some users
+// I have to have an array for tasks
+// when user clicks "add" button, I have to push task to the array
+// then clear the input
+// render the tasks array
+// delete function: ==>
 
-const users = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    age: 30,
-    isActive: true,
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    email: "janesmith@example.com",
-    age: 25,
-    isActive: false,
-  },
-  {
-    id: 3,
-    name: "Alice Johnson",
-    email: "alicejohnson@example.com",
-    age: 28,
-    isActive: true,
-  },
-  {
-    id: 4,
-    name: "Bob Brown",
-    email: "bobbrown@example.com",
-    age: 35,
-    isActive: false,
-  },
-];
+const tasks = [];
 
-const container = document.querySelector("ul");
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".add-btn");
+const h1 = document.querySelector("h1");
 
-users.forEach((person) => {
-  const li = `<li>${person.name}</li>`;
-  container.insertAdjacentHTML("beforeend", li);
+function checkTasks() {
+  if (tasks.length === 0) {
+    h1.style.visibility = "visible";
+  } else {
+    h1.style.visibility = "hidden";
+  }
+}
+
+function renderTasks() {
+  checkTasks();
+  ul.innerHTML = "";
+
+  tasks.forEach(({ id, status, title }) => {
+    const li = `<li>
+  <span># ${id}</span> 
+  <span>${title}</span> 
+   <span>${status}</span> 
+  <button id=${id}>Delete</button>
+  </li>`;
+
+    ul.insertAdjacentHTML("beforeend", li);
+  });
+
+  document.querySelectorAll("li button").forEach((btn, index) => {
+    btn.addEventListener("click", (e) => {
+      tasks.splice(index, 1);
+      renderTasks();
+    });
+  });
+}
+
+renderTasks();
+
+addBtn.addEventListener("click", () => {
+  const newTask = {
+    id: tasks.length + 1,
+    title: input.value,
+    status: "In Progress",
+  };
+  tasks.push(newTask);
+  renderTasks();
+  input.value = "";
 });
